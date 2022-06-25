81389ecc7e8e48e917279744d1501f71fe5ff72b
tohani <tohani@test.com>
2022-06-25 15:33:07 +0300

    greet function with return added

--- Lecture-1/0-Introduction/greet/function/greet_with_function.go
+++ Lecture-1/0-Introduction/greet/function/greet_with_function.go
@@ -3,9 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	greet("Tohan")
+	// prints the string directly
+	greet("Tohan") // Hello Tohan :)
+
+	// prints the string returned by the function
+	greetings := greetWithReturn("Tohan")
+	fmt.Println(greetings) // Hello Tohan :)
 }
 
+// greet function without return value
 func greet(name string) {
 	fmt.Printf("Hello %s :)\n", name)
+}
+
+// greeting function with return value
+func greetWithReturn(name string) string {
+	greeting := "Hello " + name + " :)"
+	return greeting
 }

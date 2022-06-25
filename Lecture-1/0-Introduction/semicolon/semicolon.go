abb8cadc993fcb376f5c247657d6fb91e1147b18
tohani <tohani@test.com>
2022-06-25 16:45:29 +0300

    semicolon example added

---
+++ Lecture-1/0-Introduction/semicolon/semicolon.go
@@ -0,0 +1,17 @@
+// All semicolons are redundant here!
+package main
+
+import "fmt"
+
+// All semicolons in the code are redundant in go.
+func main() {
+
+	greet := "Hello, World!"
+	fmt.Printf("%s \n", greet)
+
+	integer := 5
+	fmt.Printf("%d\n", integer)
+
+	// return in main is not required in go because it is also redundant.
+	return
+}

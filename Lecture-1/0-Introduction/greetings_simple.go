22f7919e74540062be27fdb84cb232f5a194ad03
tohani <tohani@test.com>
2022-06-25 15:13:23 +0300

    Changed folder structure

---
+++ Lecture-1/0-Introduction/greetings_simple.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Selam :)")
+	
+	// there is also 2 other ways to print a string without using fmt package
+	// printf("%s", "Selam :)")
+	// println("Selam :)")
+	// print & println are the built-in functions in Go
+}
